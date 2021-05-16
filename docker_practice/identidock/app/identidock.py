6000d9fc6d6b7219adce4b2ac0eff500a62514ac
kazunoriboy <matunorigmnw@gmail.com>
2021-05-16 15:58:54 +0900

    identiconが動くようにFlaskのソースコードを変更

--- docker_practice/identidock/app/identidock.py
+++ docker_practice/identidock/app/identidock.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
 default_name = 'Jhon Doe'
 
 @app.route('/')
-def get_identicon():
+def mainpage():
     name = default_name
 
     header = '<html><head><title>Identidock</title></head><body>'
@@ -18,5 +20,15 @@ def get_identicon():
 
     return header + body + footer
 
+@app.route('/monster/<name>')
+def get_identicon(name):
+
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    image = cache.get(name)
+    
+    return Response(image, mimetype='image/png')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
