033e7b4836a2349f5c88a5930900fb2da8c623ad
kazunoriboy <matunorigmnw@gmail.com>
2021-10-24 22:53:46 +0900

    副作用を使ったcanvasへの描画

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -1,4 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+
+function Pie(props) {
+  const { percent, color = "#ff0000" } = props;
+  const canvasRef = useRef();
+
+  useEffect(
+    () => {
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, 100, 100);
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.moveTo(50, 50);
+      ctx.arc(50, 50, 50, 0, (percent / 100) * Math.PI * 2, false);
+      ctx.lineTo(50, 50);
+      ctx.fill();
+    }
+  );
+
+  return <canvas ref={canvasRef} width={100} height={100} />;
+}
 
 function Counter(props) {
   const { count, setCount } = props;
@@ -50,6 +70,10 @@ export default function App(props) {
         <Counter count={count} setCount={setCount} />
         <Counter count={count} setCount={setCount} />
         <Counter count={count} setCount={setCount} />
+      </div>
+      <h2>Canvas</h2>
+      <div>
+        <Pie percent={80} />
       </div>
     </div>
   )
