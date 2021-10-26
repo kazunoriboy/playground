ce9fc3f27ef397e6dc69dfb21e0cd8ccd2d43fe2
kazunoriboy <matunorigmnw@gmail.com>
2021-10-26 23:54:08 +0900

    カートの状態にアクセスするための関数を定義

---
+++ react/myapp/src/state/cart/selectors.js
@@ -0,0 +1,17 @@
+export const getCartItem = (cart, id) => {
+    return cart.find((item) => item.id === id);
+};
+
+export const filterCartItem = (cart, id) => {
+    return cart.filter((item) => item.id !== id);
+};
+
+export const sortCartItems = (cart) => {
+    return cart.slice().sort((a, b) => {
+        return a.id = b.id;
+    });
+};
+
+export const checkExistItem = (cart, id) => {
+    return cart.some((item) => item.id === id);
+};
