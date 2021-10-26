7cde9db3c021573eb430c8eb1155b0a94d0315d1
kazunoriboy <matunorigmnw@gmail.com>
2021-10-26 23:41:36 +0900

    moduleのエクスポート部分だけ作成

---
+++ react/myapp/src/state/cart/index.js
@@ -0,0 +1,8 @@
+import reducer from "./reducers";
+
+import * as cartOperations from "./operations";
+import * as cartSlectors from "./selectors";
+
+export { cartOperations, cartSelectors };
+
+export default reducer;
