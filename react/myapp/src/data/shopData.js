3c7b56b1831b2c8a9ea67da3977efc764ae74165
kazunoriboy <matunorigmnw@gmail.com>
2021-10-28 00:24:00 +0900

    データを定数に格納

---
+++ react/myapp/src/data/shopData.js
@@ -0,0 +1,45 @@
+export const shopData = {
+    products: [
+        {
+            id: 1001,
+            name: "バナナ",
+            price: 200,
+            description:
+                "太さを保ちつつ長さもある大型のバナナ。デザートにどうぞ",
+            imageUrl: "/dist/images/banana.jpg",
+        },
+        {
+            id: 1002,
+            name: "リンゴ",
+            price: 150,
+            description:
+                "そのままでも、加工してリンゴ酒、ジャム、ジュース、菓子の材料などにもどうぞ。",
+            imageUrl: "/dist/images/apple.jpg",
+        },
+        {
+            id: 1003,
+            name: "オレンジ",
+            price: 100,
+            description:
+                "そのままでも、ジュース、肉料理のソースとしてもどうぞ。",
+            stock: 100,
+            imageUrl: "/dist/images/orange.jpg",
+        },
+        {
+            id: 1004,
+            name: "スイカ",
+            price: 400,
+            description:
+                "甘くて美味しい、大玉のスイカです。夏にぴったりのフルーツです。",
+            imageUrl: "/dist/images/watermelon.jpg",
+        },
+        {
+            id: 1005,
+            name: "ブドウ",
+            price: 200,
+            description:
+                "熟したブドウです。そのままでも、ソースなどにもどうぞ。",
+            imageUrl: "/dist/images/grape.png",
+        },
+    ],
+};
