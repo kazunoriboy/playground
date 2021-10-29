5394fe9e24e9f3f1685f3ee25a308679e9d0e5cc
kazunoriboy <matunorigmnw@gmail.com>
2021-10-29 09:52:33 +0900

    写経

--- react/performance-app/src/App.js
+++ react/performance-app/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  console.log('renders <App />');
+
+  const [item, setItem] = useState('');
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleChange = (event) => {
+    setItem(event.target.value);
+  };
+  
+  const handleAdd = () => {
+    if (cartItems.includes(item)) {
+      return;
+    }
+  setCartItems((items) => [...items, item]);
+  setItem('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '10px' }}>
+      <h1>商品を購入</h1>
+      <Input value={item} onChange={handleChange} />
+      <AddButton disabled={item.trim() === ''} onClick={handleAdd} />
+      <ul>
+        {cartItems.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+const Input = ({ value, onChange }) => {
+  console.log('renders <Input />');
+  
+  return (
+    <p>
+      <label>
+        アイテム名: <input type="text" value={value} onChange={onChange} />
+      </label>
+    </p>
+  );
+};
+
+const AddButton = ({ disabled, onClick }) => {
+  console.log('renders <Input />');
+
+  return (
+    <p>
+      <button disabled={disabled} onClick={onClick}>
+        カートに追加する
+      </button>
+    </p>
+  );
+};
 
 export default App;
+
