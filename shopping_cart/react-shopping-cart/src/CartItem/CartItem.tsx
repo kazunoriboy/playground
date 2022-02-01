87ed1b7a735722a4df2f84c4aaaa710362a9642e
kazunoriboy <matunorigmnw@gmail.com>
2022-02-01 16:03:59 +0900

    imageの表示忘れ

--- shopping_cart/react-shopping-cart/src/CartItem/CartItem.tsx
+++ shopping_cart/react-shopping-cart/src/CartItem/CartItem.tsx
@@ -36,6 +36,7 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
         </Button>
       </div>
     </div>
+    <img src={item.image} alt={item.title} />
   </Wrapper>
 );
 
