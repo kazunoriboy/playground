d71cbc8d4a956f2bddd7442bde913f0e50f1499e
kazunoriboy <matunorigmnw@gmail.com>
2022-02-01 15:55:27 +0900

    カートの機能実装

--- shopping_cart/react-shopping-cart/src/App.tsx
+++ shopping_cart/react-shopping-cart/src/App.tsx
@@ -29,11 +29,37 @@ const App = () => {
     getProducts
   );
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => 
+    items.reduce((ack: number, item) => ack + item.amount, 0);
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
 
-  const handleRemoveFromCart = () => null;
+      if (isItemInCart) {
+        return prev.map(item => 
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      return [...prev, { ...clickedItem, amount: 1 }]
+    });
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => 
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          console.log(item.amount - 1);
+          return [...ack, { ...item, amount: item.amount - 1}];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>somthing went wrong</div>
