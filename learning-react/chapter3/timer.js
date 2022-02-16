4956f11362a2cb083e8cee6e0d6738944445678c
kazunoriboy <matunorigmnw@gmail.com>
2022-02-16 09:42:54 +0900

    関数型の記述に書き直すため一旦全部コメントアウト

--- learning-react/chapter3/timer.js
+++ learning-react/chapter3/timer.js
@@ -1,40 +1,40 @@
-setInterval(logClockTime, 1000);
+// setInterval(logClockTime, 1000);
 
-function logClockTime() {
-  let time = getClockTime();
+// function logClockTime() {
+//   let time = getClockTime();
 
-  console.clear();
-  console.log(time);
-}
+//   console.clear();
+//   console.log(time);
+// }
 
-function getClockTime() {
-  let date = new Date();
+// function getClockTime() {
+//   let date = new Date();
 
-  let time = {
-    hours: date.getHours(),
-    minutes: date.getMinutes(),
-    seconds: date.getSeconds(),
-    ampm: "AM"
-  };
+//   let time = {
+//     hours: date.getHours(),
+//     minutes: date.getMinutes(),
+//     seconds: date.getSeconds(),
+//     ampm: "AM"
+//   };
 
-  if (time.hours == 12) {
-    time.ampm = "PM";
-  } else if (time.hours > 12) {
-    time.ampm = "PM";
-    time.hours -= 12;
-  }
+//   if (time.hours == 12) {
+//     time.ampm = "PM";
+//   } else if (time.hours > 12) {
+//     time.ampm = "PM";
+//     time.hours -= 12;
+//   }
 
-  if (time.hours < 10) {
-    time.hours = "0" + time.hours;
-  }
+//   if (time.hours < 10) {
+//     time.hours = "0" + time.hours;
+//   }
 
-  if (time.minutes < 10) {
-    time.hours = "0" + time.minutes;
-  }
+//   if (time.minutes < 10) {
+//     time.hours = "0" + time.minutes;
+//   }
 
-  if (time.seconds < 10) {
-    time.seconds = "0" + time.seconds;
-  }
+//   if (time.seconds < 10) {
+//     time.seconds = "0" + time.seconds;
+//   }
   
-  return time.hours + ":" + time.minutes + ":" + time.seconds + " " + time.ampm;
-}
+//   return time.hours + ":" + time.minutes + ":" + time.seconds + " " + time.ampm;
+// }
