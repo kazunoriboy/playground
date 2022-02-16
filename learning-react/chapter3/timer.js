cec4319b90757e98abb4a548729b0314f1575cd1
kazunoriboy <matunorigmnw@gmail.com>
2022-02-16 09:46:11 +0900

    時間に応じてAMPMの情報をオブジェクトに追加する関数

--- learning-react/chapter3/timer.js
+++ learning-react/chapter3/timer.js
@@ -54,3 +54,9 @@ const civilianHours = clockTime => ({
   ...clockTime,
   hours: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours
 });
+
+const appendAMPM = clockTime => ({
+  ...clockTime,
+  ampm: clockTime.hours >= 12 ? "PM" : "AM"
+});
+
