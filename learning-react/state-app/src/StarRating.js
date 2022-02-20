d86da1aa13620e145d5c55fa87d8eb07b26a5e1f
kazunoriboy <matunorigmnw@gmail.com>
2022-02-20 23:05:04 +0900

    stateを使って、評価の数をクリックで変更可能にする

--- learning-react/state-app/src/StarRating.js
+++ learning-react/state-app/src/StarRating.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-const Star = ({ selected = false }) => (
-  <FaStar color={selected ? "red" : "grey"} />
+const Star = ({ selected = false, onSelect = f => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
 export default function StarRating({ totalStars = 5 }) {
-  const [selectedStars] = useState(3);
+  const [selectedStars, setSelectedStars] = useState(0);
+  console.log(setSelectedStars);
   return (
     <>
       {[...Array(totalStars)].map((n, i) => (
-        <Star key={i} selected={selectedStars > i} />
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
