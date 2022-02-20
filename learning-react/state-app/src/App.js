6882db58daa3660d7003a8db4576c0729ad22579
kazunoriboy <matunorigmnw@gmail.com>
2022-02-20 23:22:07 +0900

    styleとイベントハンドラを外部から受け取るように記述を追加

--- learning-react/state-app/src/App.js
+++ learning-react/state-app/src/App.js
@@ -2,5 +2,10 @@ import React from "react";
 import StarRating from "./StarRating";
 
 export default function App() {
-  return <StarRating />
+  return (
+    <StarRating
+      style={{ backgroundColor: "lightblue" }}
+      onDoubleClick={e => alert("double click")}
+    />
+  );
 }
--- learning-react/state-app/src/StarRating.js
+++ learning-react/state-app/src/StarRating.js
@@ -5,10 +5,10 @@ const Star = ({ selected = false, onSelect = f => f }) => (
   <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
   const [selectedStars, setSelectedStars] = useState(0);
   return (
-    <>
+    <div style={{ padding: "5px", ...style }} {...props}>
       {[...Array(totalStars)].map((n, i) => (
         <Star
           key={i}
@@ -19,7 +19,7 @@ export default function StarRating({ totalStars = 5 }) {
       <p>
         {selectedStars} of {totalStars} stars
       </p>
-    </>
+    </div>
   )
     
 }
