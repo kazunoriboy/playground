09d23419b5afdb7a088e14594f30dd3aefa3280a
kazunoriboy <matunorigmnw@gmail.com>
2022-02-20 15:31:27 +0900

    指定した個数だけ星が赤くなるよう、ステートを追加

--- learning-react/state-app/src/StarRating.js
+++ learning-react/state-app/src/StarRating.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating({ totalStars = 5 }) {
-  const Star = ({ selected = false }) => (
-    <FaStar color={selected ? "red" : "grey"} />
-  );
+const Star = ({ selected = false }) => (
+  <FaStar color={selected ? "red" : "grey"} />
+);
 
-  return [...Array(totalStars)].map((_, i) => <Star key={i} />);
+export default function StarRating({ totalStars = 5 }) {
+  const [selectedStars] = useState(3);
+  return (
+    <>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  )
+    
 }
