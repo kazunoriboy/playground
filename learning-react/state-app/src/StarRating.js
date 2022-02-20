b59b4ab0593677baf60b5cb08e0ba66f74e76ac8
kazunoriboy <matunorigmnw@gmail.com>
2022-02-20 23:54:09 +0900

    ルートコンポーネントでステートを管理するため、StarRatingからステート管理の処理を削除

--- learning-react/state-app/src/StarRating.js
+++ learning-react/state-app/src/StarRating.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaStar } from "react-icons/fa";
 
 const Star = ({ selected = false, onSelect = f => f }) => (
   <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
-export default function StarRating({ style = {}, totalStars = 5, ...props }) {
-  const [selectedStars, setSelectedStars] = useState(0);
+export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
   return (
-    <div style={{ padding: "5px", ...style }} {...props}>
+    <>
       {[...Array(totalStars)].map((n, i) => (
         <Star
           key={i}
           selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
         />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
       </p>
-    </div>
+    </>
   );
 }
