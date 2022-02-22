7156d033438aeb062f33dffd3cbb0b04715b749a
kazunoriboy <matunorigmnw@gmail.com>
2022-02-22 09:56:05 +0900

    refを使用してしまうと、DOMを直接書き換えてしまうため、Stateを使って書き換え
    ちなみにこのDOMを介してデータにアクセスするコンポーネントを制御されていないコンポーネントと呼ぶ

--- learning-react/state-app/src/AddColorForm.js
+++ learning-react/state-app/src/AddColorForm.js
@@ -1,22 +1,31 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 export default function AddColorForm({ onNewColor = f => f }) {
-  const txtTitle = useRef();
-  const hexColor = useRef();
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState("#000000");
 
   const submit = e => {
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
     onNewColor(title, color);
-    txtTitle.current.value = "";
-    hexColor.current.value = "";
+    setTitle("");
+    setColor("");
   };
 
   return (
     <form onSubmit={submit}>
-      <input ref={txtTitle} type="text" placeholder="color title..." required />
-      <input ref={hexColor} type="color" required />
+      <input
+        value={title}
+        onChange={event => setTitle(event.target.value)}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input
+        value={color}
+        onChange={event => setColor(event.target.value)}
+        type="color"
+        required
+      />
       <button>ADD</button>
     </form>
   );
