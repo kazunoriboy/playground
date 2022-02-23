0d20736ebc6d1577f190f2632927c810424183a3
kazunoriboy <matunorigmnw@gmail.com>
2022-02-23 22:40:01 +0900

    ステートの管理が不要になったApp.js

--- learning-react/state-app/src/App.js
+++ learning-react/state-app/src/App.js
@@ -1,40 +1,12 @@
-import React, { useState } from "react";
-import colorData from "./color-data.json";
+import React from "react";
 import ColorList from "./ColorList.js";
 import AddColorForm from "./AddColorForm";
-import { v4 } from "uuid";
 
 export default function App() {
-  const [colors, setColors] = useState(colorData);
   return (
     <>
-      <AddColorForm 
-        onNewColor={(title, color) => {
-          const newColors = [
-            ...colors,
-            {
-              id: v4(),
-              rating: 0,
-              title,
-              color
-            }
-          ];
-          setColors(newColors);
-        }}
-      />
-      <ColorList
-        colors={colors}
-        onRateColor={(id, rating) => {
-          const newColors = colors.map(color => 
-            color.id === id ? { ...color, rating } : color
-          );
-          setColors(newColors);
-        }}
-        onRemoveColor={id => {
-          const newColors = colors.filter(color => color.id !== id);
-          setColors(newColors);
-        }}
-      />
+      <AddColorForm />
+      <ColorList />
     </>
   );
 }
