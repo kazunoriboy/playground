65597cff370c527bd08f0e753d898c2ea8da7c9e
kazunoriboy <matunorigmnw@gmail.com>
2022-02-23 22:47:21 +0900

    ColorListで使用するデータをuseContextで受け取る

--- learning-react/state-app/src/ColorList.js
+++ learning-react/state-app/src/ColorList.js
@@ -1,23 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ColorContext } from "./";
 import Color from "./Color";
 
-export default function ColorList({
-  colors = [],
-  onRemoveColor = f => f,
-  onRateColor = f => f
-}) {
+export default function ColorList() {
+  const { colors } = useContext(ColorContext);
   if (!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
   return (
     <div className="color-list">
       {
-        colors.map(color => (
-          <Color
-            key={color.id}
-            {...color}
-            onRemove={onRemoveColor}
-            onRate={onRateColor}
-          />
-        ))
+        colors.map(color => <Color key={color.id} {...color} />)
       }
     </div>
   );
