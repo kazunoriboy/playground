727d3b5e39e3cf3809ce8e43935e4e8d90a01658
kazunoriboy <matunorigmnw@gmail.com>
2022-02-25 09:50:01 +0900

    カスタムフックを使用する側のコードを記述

--- learning-react/state-app/src/useEffect/index.js
+++ learning-react/state-app/src/useEffect/index.js
@@ -16,3 +16,16 @@ const useJazzyNews = () => {
 
   return posts;
 }
+
+function NewsFeed({ url }) {
+  const posts = useJazzyNews();
+
+  return (
+    <>
+      <h1>{posts.length} articles</h1>
+      {posts.map(post => (
+        <Post key={post.id} {...post} />
+      ))}
+    </>
+  )
+}
