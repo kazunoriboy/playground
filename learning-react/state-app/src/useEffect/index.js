063f8608d703cc67ad77d81f822471c345285554
kazunoriboy <matunorigmnw@gmail.com>
2022-02-25 09:44:04 +0900

    useEffect内でリターンすると、リターンの右辺の関数をコンポーネントのアンマウント時に実行することができる

--- learning-react/state-app/src/useEffect/index.js
+++ learning-react/state-app/src/useEffect/index.js
@@ -1,40 +1,11 @@
 import React, { useEffect } from "react";
 
-function App() {
-  const [val, set] = useState("");
-  const [phrase, setPhrase] = useSate("example phrase");
-
-  const createPhrase = () => {
-    setPhrase(val);
-    set("");
-  };
+const useJazzyNews = () => {
+  const [posts, setPosts] = useState([]);
+  const addPost = post => setPosts(allPosts => [post, ...allPosts]);
 
   useEffect(() => {
-    console.log("either val or phrase has changed");
-  }, [val, phrase]);
-
-  useEffect(() => {
-    console.log("only once after initial render");
-  }, []);
-
-  useEffect(() => {
-    console.log(`typing "${val}"`);
-  }, [val]);
-
-  useEffect(() => {
-    console.log(`saved phrase: "${phrase}"`);
-  }, [phrase]);
-
-  return (
-    <>
-      <label>Favorite Phrase</label>
-      <input
-        type="text"
-        value={val}
-        placeholder={phrase}
-        onChange={e => set(e.target.value)}
-      />
-      <button onClick={createPhrase}>send</button>
-    </>
-  );
+    newsFeed.subscribe(addPost);
+    return () => newsFeed.unsubscribe(addPost);
+  });
 }
