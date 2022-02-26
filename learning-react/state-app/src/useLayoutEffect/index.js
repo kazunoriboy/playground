99b025f5c5ababa6ef8be805dcc8394a6664a7d0
kazunoriboy <matunorigmnw@gmail.com>
2022-02-26 22:15:05 +0900

    useLayoutEffectの使用例：画面描画の直前で画面の横幅と高さを元にコンポーネントのサイズを計算するカスタムフック

--- learning-react/state-app/src/useLayoutEffect/index.js
+++ learning-react/state-app/src/useLayoutEffect/index.js
@@ -5,3 +5,21 @@ export function App() {
   useLayoutEffect(() => console.log("useLayoutEffect"));
   return <div>ready</div>
 }
+
+function useWindowSize() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const resize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("resize", resize);
+    resize();
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  return [width, height];
+}
