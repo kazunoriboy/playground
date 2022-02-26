83478f60f499cc63ca92838ffd809bbcc68215ac
kazunoriboy <matunorigmnw@gmail.com>
2022-02-26 21:58:38 +0900

    変数をメモ化し、投稿がある度に副作用が実行されるように変更

--- learning-react/state-app/src/useEffect/index.js
+++ learning-react/state-app/src/useEffect/index.js
@@ -1,43 +1,49 @@
 import React, { useState, useEffect, useMemo, useCallback } from "react";
 
-// const useJazzyNews = () => {
-//   const [posts, setPosts] = useState([]);
-//   const addPost = post => setPosts(allPosts => [post, ...allPosts]);
+const useJazzyNews = () => {
+  const [_posts, setPosts] = useState([]);
+  const addPost = post => setPosts(allPosts => [post, ...allPosts]);
 
-//   useEffect(() => {
-//     newsFeed.subscribe(addPost);
-//     return () => newsFeed.unsubscribe(addPost);
-//   }, []);
-
-//   useEffect(() => {
-//     welcomeChime.play();
-//     return () => goodbyeChime.play();
-//   }, []);
-
-//   return posts;
-// }
-
-// function NewsFeed({ url }) {
-//   const posts = useJazzyNews();
-
-//   return (
-//     <>
-//       <h1>{posts.length} articles</h1>
-//       {posts.map(post => (
-//         <Post key={post.id} {...post} />
-//       ))}
-//     </>
-//   )
-// }
-
-const useAnyKeyToRender = () => {
-  const [, forceRender] = useState();
+  const posts = useMemo(() => _posts, [_posts]);
 
   useEffect(() => {
-    window.addEventListener("keydown", forceRender);
-    return () => window.removeEventListener("keydown", forceRender)
-  })
+    newPostChime.play();
+  }, [posts]);
+
+  useEffect(() => {
+    newsFeed.subscribe(addPost);
+    return () => newsFeed.unsubscribe(addPost);
+  }, []);
+
+  useEffect(() => {
+    welcomeChime.play();
+    return () => goodbyeChime.play();
+  }, []);
+
+  return posts;
 }
+
+function newsFeed({ url }) {
+  const posts = useJazzyNews();
+
+  return (
+    <>
+      <h1>{posts.length} articles</h1>
+      {posts.map(post => (
+        <Post key={post.id} {...post} />
+      ))}
+    </>
+  )
+}
+
+// const useAnyKeyToRender = () => {
+//   const [, forceRender] = useState();
+
+//   useEffect(() => {
+//     window.addEventListener("keydown", forceRender);
+//     return () => window.removeEventListener("keydown", forceRender)
+//   })
+// }
 
 // const word = ["sick", "powder", "day"];
 // export function App() {
@@ -51,32 +57,32 @@ const useAnyKeyToRender = () => {
 // }
 
 
-function WordCount({ children = "" }) {
-  useAnyKeyToRender();
+// function WordCount({ children = "" }) {
+//   useAnyKeyToRender();
 
-  const words = useMemo(() => children.split(" "), [children]);
+//   const words = useMemo(() => children.split(" "), [children]);
 
-  const fn = useCallback(() => {
-    console.log("hello");
-    console.log("world");
-  }, []);
+//   const fn = useCallback(() => {
+//     console.log("hello");
+//     console.log("world");
+//   }, []);
   
-  useEffect(() => {
-    console.log("fresh render");
-    fn();
-  }, [fn]);
+//   useEffect(() => {
+//     console.log("fresh render");
+//     fn();
+//   }, [fn]);
 
-  return (
-    <>
-      <p>{children}</p>
-      <p>
-        <strong>{words.length} - words</strong>
-      </p>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <p>{children}</p>
+//       <p>
+//         <strong>{words.length} - words</strong>
+//       </p>
+//     </>
+//   );
+// }
 
-export function App() {
-  return <WordCount>You are not going to believe this but...</WordCount>;
-}
+// export function App() {
+//   return <WordCount>You are not going to believe this but...</WordCount>;
+// }
 
