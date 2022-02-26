f5eaab450be30c353205305a72af3179675f45cc
kazunoriboy <matunorigmnw@gmail.com>
2022-02-26 22:20:24 +0900

    useLayoutEffectの使用例：マウスカーソルの座標を追跡するカスタムフック

--- learning-react/state-app/src/useLayoutEffect/index.js
+++ learning-react/state-app/src/useLayoutEffect/index.js
@@ -23,3 +23,20 @@ function useWindowSize() {
 
   return [width, height];
 }
+
+function useMousePosition() {
+  const [x, setX] = setState(0);
+  const [y, setY] = useState(0);
+
+  const setPosition = ({ x, y }) => {
+    setX(x);
+    setY(y);
+  }
+
+  useLayoutEffect = () => {
+    window.addEventListener("mousemove", setPosition);
+    return () => window.removeEventListener("mousemove", setPosition);
+  }
+
+  return [x, y];
+}
