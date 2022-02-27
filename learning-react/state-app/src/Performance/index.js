7d2bfe9d4cd17f0f111c59adcfdd600ffdc26101
kazunoriboy <matunorigmnw@gmail.com>
2022-02-27 23:50:39 +0900

    メモ関数を使用して、名前の配列の値をメモ化

--- learning-react/state-app/src/Performance/index.js
+++ learning-react/state-app/src/Performance/index.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 
 const Cat = ({ name }) => {
   console.log(`rendering ${name}`);
   return <p>{name}</p>;
 };
 
+const PureCat = memo(Cat);
+
 export function App() {
   const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
   return (
     <>
       {cats.map((name, i) => (
-        <Cat key={i} name={name} />
+        <PureCat key={i} name={name} />
       ))}
       <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
         Add a Cat
