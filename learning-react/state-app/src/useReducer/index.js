ff8c35bf82ce60376b43ed131bd9024fec6d79af
kazunoriboy <matunorigmnw@gmail.com>
2022-02-27 22:38:46 +0900

    複雑なオブジェクトを表示するコンポーネントを作成

--- learning-react/state-app/src/useReducer/index.js
+++ learning-react/state-app/src/useReducer/index.js
@@ -18,3 +18,30 @@ function CheckBox() {
     </>
   );
 }
+
+const firstUser = {
+  id: "0391-3233-3201",
+  firstName: "Bill",
+  lastName: "Wilson",
+  city: "Missoula",
+  state: "Montana",
+  email: "bwilson@mtnwilsons.com",
+  admin: false
+};
+
+function User() {
+  const [user, setUser] = useState(firstUser);
+
+  return (
+    <div>
+      <h1>
+        {user.firstName} {user.lastName} - {user.admin ? "Admin" : "User"}
+      </h1>
+      <p>Email: {user.email}</p>
+      <p>
+        Location: {user.city}, {user.state}
+      </p>
+      <button>Make Admin</button>
+    </div>
+  );
+}
