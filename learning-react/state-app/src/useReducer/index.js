0ddc82121a2aed09a834beba757cf5075fba247f
kazunoriboy <matunorigmnw@gmail.com>
2022-02-27 22:27:30 +0900

    返り値は不要なので関数に切り分け

--- learning-react/state-app/src/useReducer/index.js
+++ learning-react/state-app/src/useReducer/index.js
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 function CheckBox() {
   const [checked, setChecked] = useState(false);
 
+  function toggle() {
+    setChecked(checked => !checked)
+  }
+
   return (
     <>
       <input
         type="checkbox"
         value={checked}
-        onChange={() => setChecked(checked => !checked)}
+        onChange={toggle}
       />
       {checked ? "checked" : "not checked"}
     </>
