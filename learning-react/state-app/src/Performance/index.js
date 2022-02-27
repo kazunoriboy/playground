bd8785d489330da99ddfe40e08e6580b8ba95877
kazunoriboy <matunorigmnw@gmail.com>
2022-02-28 00:01:53 +0900

    こっちのメモ関数はいらない説

--- learning-react/state-app/src/Performance/index.js
+++ learning-react/state-app/src/Performance/index.js
@@ -1,9 +1,9 @@
 import React, { useState, memo } from "react";
 
-const Cat = memo(({ name, meow = f => f }) => {
+const Cat = ({ name, meow = f => f }) => {
   console.log(`rendering ${name}`);
   return <p onClick={() => meow(name)}>{name}</p>;
-});
+};
 
 const PureCat = memo(
   Cat,
