4679c6baaf025bc83631777b474ae3e349865705
kazunoriboy <matunorigmnw@gmail.com>
2022-02-28 00:09:19 +0900

    useCallBackフックを使用すれば、predicateの計算式を書かなくても関数プロパティをメモ化できる

--- learning-react/state-app/src/Performance/index.js
+++ learning-react/state-app/src/Performance/index.js
@@ -1,21 +1,19 @@
-import React, { useState, memo } from "react";
+import React, { useState, memo, useCallback } from "react";
 
 const Cat = ({ name, meow = f => f }) => {
   console.log(`rendering ${name}`);
   return <p onClick={() => meow(name)}>{name}</p>;
 };
 
-const PureCat = memo(
-  Cat,
-  (prevProps, nextProps) => prevProps.name === nextProps.name
-);
+const PureCat = memo(Cat);
 
 export function App() {
+  const meow = useCallback(name => console.log(`${name} has meowed`), []);
   const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
   return (
     <>
       {cats.map((name, i) => (
-        <PureCat key={i} name={name} meow={name => console.log(`${name} has meowed`)} />
+        <PureCat key={i} name={name} meow={meow} />
       ))}
       <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
         Add a Cat
