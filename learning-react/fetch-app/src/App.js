f8fe0c885f35d7ba135902b918229ec5b9b959f0
kazunoriboy <matunorigmnw@gmail.com>
2022-03-02 09:44:49 +0900

    githubから取得した情報を表示して、ローカルストレージへ保存するコンポーネントを作成
    すでにローカルストレージに値がある時はそちらを取得する

--- learning-react/fetch-app/src/App.js
+++ learning-react/fetch-app/src/App.js
@@ -3,24 +3,38 @@ import React, { useState, useEffect } from "react";
 const loadJSON = key => 
   key && JSON.parse(localStorage.getItem(key));
 
-const saveJSON = (key, data) => 
+const saveJSON = (key, data) => {
   localStorage.setItem(key, JSON.stringify(data));
+}
 
 function GitHubUser({ login }) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(
+    loadJSON(`user:${login}`)
+  );
+
+  useEffect(() => {
+    if (!data) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
+    if (data && data.login === login) return;
     fetch(`https://api.github.com/users/${login}`)
       .then(response => response.json())
       .then(setData)
       .catch(console.error);
-  }, [login]);
+    }, [login]);
 
   if (data) {
     return <pre>{JSON.stringify(data, null, 2)}</pre>;
   }
-
   return null;
 }
 
