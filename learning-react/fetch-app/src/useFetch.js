13bf6e618f674ffc93abd78252ad4212ab05cd12
kazunoriboy <matunorigmnw@gmail.com>
2022-03-04 10:12:33 +0900

    fetch用のカスタムフックを作成

---
+++ learning-react/fetch-app/src/useFetch.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+export function useFetch(uri) {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!uri) return;
+    fetch(uri)
+      .then(data => data.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [uri]);
+
+  return {
+    loading,
+    data,
+    error
+  };
+}
