d5264a11530f22c75c57b43478a805765e734eca
kazunoriboy <matunorigmnw@gmail.com>
2022-03-04 10:26:29 +0900

    ファイル名を変更

--- learning-react/fetch-app/src/App.js
+++ learning-react/fetch-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useFetch } from "./useFetch";
+import { useFetch } from "./Fetch";
 import SearchForm from "./SearchForm";
 
 const loadJSON = key => 
