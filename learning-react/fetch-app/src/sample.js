b63a43e8f843b9dc2715d363c00215519505fe25
kazunoriboy <matunorigmnw@gmail.com>
2022-03-04 09:46:36 +0900

    fakerで大量のデータを描画するコンポーネント

--- learning-react/fetch-app/src/sample.js
+++ learning-react/fetch-app/src/sample.js
@@ -1,14 +1,9 @@
 import React from "react";
-
-const tahoe_peaks = [
-  { name: "Freel Peak", elevation: 10891 },
-  { name: "Monument Peak", elevation: 10067 },
-  { name: "Pyramid Peak", elevation: 9983 },
-  { name: "Mt. Tallac", elevation: 9735 }
-];
+import { FixedSizeList } from "react-window";
+import { faker } from "@faker-js/faker";
 
 function List({ data = [], renderItem, renderEmpty }) {
-  !data.length ? (
+  return !data.length ? (
     renderEmpty
   ) : (
     <ul>
@@ -19,16 +14,27 @@ function List({ data = [], renderItem, renderEmpty }) {
   );
 }
 
+const bigList = [...Array(5000)].map(() => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar()
+}));
+
 export default function App() {
+  const renderItem = item => (
+    <div style={{ display: "flex" }}>
+      <img src={item.avatar} alt={item.name} width={50} />
+      <p>
+        {item.name} - {item.email}
+      </p>
+    </div>
+  );
+
   return (
     <List
-      data={tahoe_peaks}
+      data={bigList}
       renderEmpty={<p>This list is empty</p>}
-      renderItem={item => (
-        <>
-          {item.name} - {item.elevation.toLocaleString()}
-        </>
-      )}
+      renderItem={renderItem}
     />
   );
 }
