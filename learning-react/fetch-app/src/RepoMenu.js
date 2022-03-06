6cc9a4adbb2c71694a30d338f2675589e211fc34
kazunoriboy <matunorigmnw@gmail.com>
2022-03-06 22:50:07 +0900

    useIteratorを使ってRepoMenuコンポーネントを作成

---
+++ learning-react/fetch-app/src/RepoMenu.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { useIterator } from "./Iterator";
+
+export function RepoMenu({
+  repositories,
+  onSelect = f => f
+}) {
+  const [{ name }, previous, next] = useIterator(repositories);
+
+  useEffect(() => {
+    if (!name) return;
+    onSelect(name);
+  }, [name]);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <button onClick={previous}>&lt;</button>
+      <p>{name}</p>
+      <button onClick={next}>&gt;</button>
+    </div>
+  );
+}
