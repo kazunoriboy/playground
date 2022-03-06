28db71783ca151e41eec67b82cf984c23a85cffc
kazunoriboy <matunorigmnw@gmail.com>
2022-03-06 22:49:47 +0900

    関数と値をメモ化

--- learning-react/fetch-app/src/Iterator.js
+++ learning-react/fetch-app/src/Iterator.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 export const useIterator = (
   items = [],
@@ -6,15 +6,17 @@ export const useIterator = (
 ) => {
   const [i, setIndex] = useState(initialIndex);
 
-  const prev = () => {
+  const prev = useCallback(() => {
     if (i === 0) return setIndex(items.length - 1);
     setIndex(i - 1);
-  };
+  }, [i]);
 
-  const next = () => {
+  const next = useCallback(() => {
     if (i === items.length - 1) return setIndex(0);
     setIndex(i + 1);
-  };
+  }, [i]);
 
-  return [items[i], prev, next];
+  const item = useMemo(() => items[i], [i]);
+
+  return [item || items[0], prev, next];
 }
