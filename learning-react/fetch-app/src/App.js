73cb08ecc222576a16acada48ca309f510d85977
kazunoriboy <matunorigmnw@gmail.com>
2022-03-09 23:04:06 +0900

    今まで作成したコンポーネントをappで読み込む構造に変更

--- learning-react/fetch-app/src/App.js
+++ learning-react/fetch-app/src/App.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
-import UserRepositories from "./UserRepositories";
 import SearchForm from "./SearchForm"; 
 import GitHubUser from "./GitHubUser";
+import UserRepositories from "./UserRepositories";
+import RepositoryReadme from "./RepositoryReadme";
 
 export default function App() {
   const [login, setLogin] = useState("kazunoriboy");
+  const [repo, setRepo] = useState("playground");
 
   return (
     <>
       <SearchForm value={login} onSearch={setLogin} />
       <GitHubUser login={login} />
+      <UserRepositories 
+        login={login}
+        selectedRepo={repo}
+        onSelect={setRepo}
+      />
+      <RepositoryReadme login={login} repo={repo} />
     </>
-  )
+  );
 }
 
