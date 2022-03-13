acc696048d5493d18300c1558f42f8e967d0b058
kazunoriboy <matunorigmnw@gmail.com>
2022-03-13 20:02:12 +0900

    同意確認のコンポーネントを作成

---
+++ learning-react/suspense-app/src/Agreement.js
@@ -0,0 +1,9 @@
+export default function Agreement({ onAgree = f => f }) {
+  return (
+    <div>
+      <p>Terms...</p>
+      <p>These are the terms and stuff. Do you agree?</p>
+      <button onClick={onAgree}>I agree</button>
+    </div>
+  );
+}
