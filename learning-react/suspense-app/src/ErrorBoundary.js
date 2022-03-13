d5820955baff4df12b685baa06e59b4c10ea6be0
kazunoriboy <matunorigmnw@gmail.com>
2022-03-13 19:34:20 +0900

    エラーバウンダリーを定義しAppコンポーネント内の例外を補足できるように変更

---
+++ learning-react/suspense-app/src/ErrorBoundary.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+  state = { error: null };
+
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { children, fallback } = this.props;
+
+    if (error) return fallback({error});
+    return children;
+  }
+}
--- learning-react/suspense-app/src/index.js
+++ learning-react/suspense-app/src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary';
+
+function ErrorScreen({ error }) {
+  return (
+    <div className="error">
+      <h3>We are sorry ... something went wrong</h3>
+      <p>We cannnot process your request at this moment.</p>
+      <p>ERROR: {error.message}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={ErrorScreen}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
