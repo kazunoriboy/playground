1368147549b84c336cb56db9ab9d47ae8c4ad820
kazunoriboy <matunorigmnw@gmail.com>
2022-03-13 19:41:08 +0900

    fallbackのデフォルトを作成

--- learning-react/suspense-app/src/ErrorBoundary.js
+++ learning-react/suspense-app/src/ErrorBoundary.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 
+function ErrorScreen({ error }) {
+  return (
+    <div className="error">
+      <h3>We are sorry ... something went wrong</h3>
+      <p>We cannnot process your request at this moment.</p>
+      <p>ERROR: {error.message}</p>
+    </div>
+  );
+}
+
 export default class ErrorBoundary extends Component {
   state = { error: null };
 
@@ -11,7 +21,9 @@ export default class ErrorBoundary extends Component {
     const { error } = this.state;
     const { children, fallback } = this.props;
 
+    if (error && !fallback) return <ErrorScreen error={error} />
     if (error) return fallback({error});
+
     return children;
   }
 }
--- learning-react/suspense-app/src/index.js
+++ learning-react/suspense-app/src/index.js
@@ -4,19 +4,9 @@ import './index.css';
 import App from './App';
 import ErrorBoundary from './ErrorBoundary';
 
-function ErrorScreen({ error }) {
-  return (
-    <div className="error">
-      <h3>We are sorry ... something went wrong</h3>
-      <p>We cannnot process your request at this moment.</p>
-      <p>ERROR: {error.message}</p>
-    </div>
-  );
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary fallback={ErrorScreen}>
+    <ErrorBoundary>
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
