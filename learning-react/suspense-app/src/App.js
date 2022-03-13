faf8073e891acc310aa7cfae555bf65d67f3ae22
kazunoriboy <matunorigmnw@gmail.com>
2022-03-13 18:42:37 +0900

    アプリケーションのベースレイアウトを作成

--- learning-react/suspense-app/src/App.js
+++ learning-react/suspense-app/src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import SiteLayout from './SiteLayout';
+
+const Callout = ({ children }) => (
+  <div className="callout">{children}</div>
+);
 
 function App() {
-
+  return (
+    <SiteLayout menu={<p>Menu</p>}>
+      <>
+        <Callout>Callout</Callout>
+        <h1>Contents</h1>
+        <p>This is the main part of the exmaple layout</p>
+      </>
+    </SiteLayout>
+  );
 }
 
 export default App;
