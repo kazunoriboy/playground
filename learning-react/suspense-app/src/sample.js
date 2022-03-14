e43fe105ab0fc6286d60b1a132006aa8d2bb3681
kazunoriboy <matunorigmnw@gmail.com>
2022-03-14 18:00:10 +0900

    prettierのインストールと設定

--- learning-react/suspense-app/src/sample.js
+++ learning-react/suspense-app/src/sample.js
@@ -20,6 +20,8 @@
 //   return <h1>gnarly</h1>
 // }
 
-function Image() {
-  return <img src="/img.png" />;
-}
+// function Image() {
+//   return <img src="/img.png" />;
+// }
+
+console.log("Prettier Test");
