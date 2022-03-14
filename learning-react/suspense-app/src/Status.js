b818bb337e458f14f1853eb6f49fe59bd930caaf
kazunoriboy <matunorigmnw@gmail.com>
2022-03-14 15:42:03 +0900

    promiseをthrow出来るかどうかのテスト

--- learning-react/suspense-app/src/Status.js
+++ learning-react/suspense-app/src/Status.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 const loadStatus = () => {
-  throw new Promise(resolves => null);
+  console.log("load status");
+  throw new Promise(resolves => setTimeout(resolves, 3000));
 };
+
+safe(loadStatus);
+
+function safe(fn) {
+  try {
+    fn();
+  } catch (error) {
+    if (error instanceof Promise) {
+      error.then(() => safe(fn));
+    } else {
+      throw error;
+    }
+  }
+}
 
 export default function Status() {
   const status = loadStatus();
