945fd5bb2a37107288779fa57a990cfbae5ffebe
kazunoriboy <matunorigmnw@gmail.com>
2022-03-14 17:49:43 +0900

    eslintプラグインを追加

--- learning-react/suspense-app/src/sample.js
+++ learning-react/suspense-app/src/sample.js
@@ -1,16 +1,21 @@
-const gnar = "gnarly";
+// const gnar = "gnarly";
 
-const info = ({
-  file = __filename,
-  dir = __dirname
-}) => (
-  <p>
-    {dir}: {file}
-  </p>
-);
+// const info = ({
+//   file = __filename,
+//   dir = __dirname
+// }) => (
+//   <p>
+//     {dir}: {file}
+//   </p>
+// );
 
-switch (gnar) {
-  default:
-    console.log("gnarly");
-    break;
+// switch (gnar) {
+//   default:
+//     console.log("gnarly");
+//     break;
+// }
+
+function gnar() {
+  const [nickname, setNickname] = useState("dude");
+  return <h1>gnarly</h1>
 }
