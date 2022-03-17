d8f6c1e0b99c80dd0da555ecbb43bbb1cde79dc5
kazunoriboy <matunorigmnw@gmail.com>
2022-03-17 09:32:09 +0900

    配列の値をテストするコードを作成

--- learning-react/testing/src/functions.js
+++ learning-react/testing/src/functions.js
@@ -1,3 +1,7 @@
 export function timesTwo(a) {
   return a * 2;
 }
+
+export function order() {
+  /* ... */
+}
--- learning-react/testing/src/functions.test.js
+++ learning-react/testing/src/functions.test.js
@@ -3,3 +3,11 @@ import { timesTwo } from "./functions";
 test("Multiplies by two", () => {
   expect(timesTwo(4)).toBe(8);
 });
+
+test("Build an order object", () => {
+  const result = {
+    orderItems: menuItems,
+    total: 77,
+  };
+  expect(order(menuItems)).toEqual(result);
+});
