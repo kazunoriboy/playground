b6de4dbac943faa0a358224dd53baf1dab4b041e
kazunoriboy <matunorigmnw@gmail.com>
2022-03-17 09:43:12 +0900

    配列のデータを元にオブジェクトを生成するコードを作成

--- learning-react/testing/src/functions.js
+++ learning-react/testing/src/functions.js
@@ -2,6 +2,10 @@ export function timesTwo(a) {
   return a * 2;
 }
 
-export function order() {
-  /* ... */
+export function order(items) {
+  const total = items.reduce((price, item) => price + item.price, 0);
+  return {
+    orderItems: items,
+    total: total,
+  };
 }
