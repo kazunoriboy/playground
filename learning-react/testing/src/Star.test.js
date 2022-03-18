e7ba51780c89885ac517ccdc292164ae84537048
kazunoriboy <matunorigmnw@gmail.com>
2022-03-18 20:41:59 +0900

    render関数を使用したh1要素のテスト

--- learning-react/testing/src/Star.test.js
+++ learning-react/testing/src/Star.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Star from "./Star";
+import { render } from "@testing-library/react";
 
 test("renders a star", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Star />, div);
   expect(div.querySelector("svg")).toHaveAttribute("id", "star");
+});
+
+test("reders an h1", () => {
+  const { getByText } = render(<Star />);
+  const h1 = getByText(/Great Star/);
+  expect(h1).toHaveTextContent("Great Star");
 });
