9ca6c567384dc4f0561fc25cacceb9cb89e1c1f8
kazunoriboy <matunorigmnw@gmail.com>
2022-03-18 20:30:30 +0900

    カスタムマッチャーを使ったテストを実行

--- learning-react/testing/src/Star.test.js
+++ learning-react/testing/src/Star.test.js
@@ -5,5 +5,5 @@ import Star from "./Star";
 test("renders a star", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Star />, div);
-  expect(div.querySelector("svg")).toBeTruthy();
+  expect(div.querySelector("svg")).toHaveAttribute("id", "star");
 });
