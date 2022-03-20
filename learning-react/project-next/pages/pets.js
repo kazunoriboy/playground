16427cb5921ac1816abe1e05a7c5fdd7237843e2
kazunoriboy <matunorigmnw@gmail.com>
2022-03-20 22:09:48 +0900

    外部サーバーからフェッチしたデータを初期データとして埋め込んで表示

--- learning-react/project-next/pages/pets.js
+++ learning-react/project-next/pages/pets.js
@@ -1,9 +1,26 @@
 import Layout from "./Layout";
+import fetch from "isomorphic-fetch";
 
-export default function Pets() {
+export default function Pets(props) {
   return (
     <Layout>
-      <h1>Pets!</h1>
+      <div>
+        <h1>Pets!</h1>
+        <ul>
+          {props.pets.map((pet) => (
+            <li key={pet.id}>{pet.name}</li>
+          ))}
+        </ul>
+      </div>
     </Layout>
   );
 }
+
+Pets.getInitialProps = async function () {
+  const res = await fetch(`http://pet-library.moonhighway.com/api/pets`);
+  const data = await res.json();
+  console.log(data);
+  return {
+    pets: data,
+  };
+};
