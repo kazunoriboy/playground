93445cdac1db45ea8e301dade2143b278b7e27ce
kazunoriboy <matunorigmnw@gmail.com>
2022-03-21 16:18:07 +0900

    関数スコープに閉じ込める

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -1,12 +1,14 @@
 console.log('Hello TypeScript');
 
-let a = 1 + 2
-let b = a + 3
-let c = {
-  apple: a,
-  banana: b
+function typeInference() {
+  let a = 1 + 2
+  let b = a + 3
+  let c = {
+    apple: a,
+    banana: b
+  }
+  let d = c.apple * 4
 }
-let d = c.apple * 4
 
 function squareOf(n: number) {
   return n * n
