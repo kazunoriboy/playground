eacfa3bf130a14e5f49e4319aac80cf36e32ab9c
kazunoriboy <matunorigmnw@gmail.com>
2022-03-21 19:24:28 +0900

    欠如を表す方のバリエーション

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -284,3 +284,25 @@ function tupleSample() {
   type D = readonly [number, string]
   type E = Readonly<[number, string]>
 }
+
+function nullUndefinedVoidNeverSample() {
+  function a(x: number) {
+    if (x < 10) {
+      return x
+    }
+    return null
+  }
+
+  function b() {
+    return undefined
+  }
+
+  function c() {
+    let a = 2 + 2
+    let b = a * a
+  }
+
+  function d() {
+    throw TypeError('I always error')
+  }
+}
