29a62137c4d9e9e739bd9fe567e08fd1cbc7e19d
kazunoriboy <matunorigmnw@gmail.com>
2022-03-21 16:55:07 +0900

    stringの型アノテーション。ルールはだいたい他のプリミティブと同じ

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -61,3 +61,13 @@ function bigIntSample() {
   let g: 100n = 100n
   let h: bigint = 100
 }
+
+function stringSample() {
+  let a = 'hello'
+  var b = 'billy'
+  const c = '!'
+  let d = a + ' ' + b + c
+  let e: string = 'zoom'
+  let f: 'john' = 'john'
+  let g: 'john' = 'zoe'
+}
