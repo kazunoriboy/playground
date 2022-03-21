f2e2e09d3a73089d98370bfd2ed979ce8c6c07b3
kazunoriboy <matunorigmnw@gmail.com>
2022-03-21 19:12:14 +0900

    可変長のタプルを宣言

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -263,7 +263,10 @@ function tupleSample() {
 
   let trainFares: [number, number?][] = [
     [3.75],
-    [8.25, 7.70]
+    [8.25, 7.70],
     [10.50]
   ]
+
+  let friends: [string, ...string[]] = ['Sara', 'Tali', 'Chloe', 'Claire']
+  let list: [number, boolean, ...string[]] = [1, false, 'a', 'b', 'c']
 }
