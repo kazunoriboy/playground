47e8758ffe297f0d6d419670312de42e49fcdf73
kazunoriboy <matunorigmnw@gmail.com>
2022-03-21 22:53:41 +0900

    練習問題

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -356,3 +356,26 @@ function enumSample() {
   flip(Flippable.Cup)
   flip(12)
 }
+
+function practice() {
+  let a = 1042
+  let b = 'apples and oranges'
+  const c = 'pineapples'
+  let d = [true, true, false]
+  let e = { type: 'ficus' }
+  let f = [1, false]
+  const g = [3]
+  let h = null
+
+  let i: 3 = 3
+  i = 4
+
+  let j = [1, 2, 3]
+  j.push(4)
+  j.push('5')
+
+  let k: never = 4
+
+  let l: unknown = 4
+  let m = l * 2
+}
