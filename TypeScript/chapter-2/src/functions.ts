09470554d56ae6e9c8cc03f2f77ea0410a4296f2
kazunoriboy <matunorigmnw@gmail.com>
2022-03-22 09:10:36 +0900

    関数定義の型アノテーションのパターン。関数コンストラクタは型安全ではないため非推奨。

--- TypeScript/chapter-2/src/functions.ts
+++ TypeScript/chapter-2/src/functions.ts
@@ -1,3 +1,21 @@
 function add(a: number, b: number): number {
   return a + b
 }
+
+function greet(name: string) {
+  return 'hello' + name
+}
+
+let greet2 = function (name: string) {
+  return 'hello' + name
+}
+
+let greet3 = (name: string) => {
+  return 'hello' + name
+}
+
+let greet4 = (name: string) => {
+  'hello' + name
+}
+
+let greet5 = new Function('name', 'return "hellot " + name')
