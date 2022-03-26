07e2a8f239a58e9cf65a7a4b37952b9f28b17f96
kazunoriboy <matunorigmnw@gmail.com>
2022-03-26 19:44:20 +0900

    二分木の型エイリアスとそれを生成するmapNodeの呼び出しを満たす関数定義を作成（自作）

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -426,3 +426,25 @@ triggerEvent({
   target: document.querySelector('#myButton'),
   type: 'mouseover'
 })
+
+type TreeNode = {
+  value: string
+}
+type LeafNode = TreeNode & {
+  isLeaf: true
+}
+type InnerNode = TreeNode & {
+  children: [TreeNode] | [TreeNode, TreeNode]
+}
+
+let a: TreeNode = { value: 'a' }
+let b: LeafNode = { value: 'b', isLeaf: true }
+let c: InnerNode = { value: 'c', children: [b] }
+
+let a1 = mapNode(a, _ => _.toUpperCase())
+let b1 = mapNode(b, _ => _.toUpperCase())
+let c1 = mapNode(c, _ => _.toUpperCase())
+
+function mapNode<T>(node: T, f: (value: string) => string) {
+
+}
