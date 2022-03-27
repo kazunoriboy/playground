a0d02efde1c3f0433e43d53901f3b6c2fcc1e94e
kazunoriboy <matunorigmnw@gmail.com>
2022-03-27 09:03:27 +0900

    復数の制約を持つ制限付きポリモーフィズムの定義

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -454,3 +454,15 @@ function mapNode<T extends TreeNode>(
     value: f(node.value)
   }
 }
+
+type HasSides = { numberOfSides: number }
+type SidesHaveLength = { sideLength: number }
+
+function logPerimeter<Shape extends HasSides & SidesHaveLength>(s: Shape): Shape {
+  console.log(s.numberOfSides * s.sideLength);
+  return s
+}
+
+type Square = HasSides & SidesHaveLength
+let square: Square = { numberOfSides: 4, sideLength: 3 }
+logPerimeter(square)
