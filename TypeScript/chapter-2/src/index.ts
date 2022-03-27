25d8bf5c226bee70b0152bdf2592f27f324f68ca
kazunoriboy <matunorigmnw@gmail.com>
2022-03-27 09:41:10 +0900

    制限付きポリモーフィズムを使って可変長引数の関数を作成する

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -466,3 +466,16 @@ function logPerimeter<Shape extends HasSides & SidesHaveLength>(s: Shape): Shape
 type Square = HasSides & SidesHaveLength
 let square: Square = { numberOfSides: 4, sideLength: 3 }
 logPerimeter(square)
+
+function call<T extends unknown[], R>(
+  f: (...args: T) => R,
+  ...args: T
+): R {
+  return f(...args)
+}
+
+function fill(length: number, value: string): string[] {
+  return Array.from({ length }, () => value)
+}
+
+call(fill, 10, 'a')
