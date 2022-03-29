86aecf981dfc327281dd141a972f6e9eb938d57d
kazunoriboy <matunorigmnw@gmail.com>
2022-03-29 09:11:26 +0900

    チェスのクラスを作成

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -526,3 +526,34 @@ let Reserve: Reserve = (
   }
 }
 
+class Game { }
+
+class Piece {
+  protected position: Position
+
+  constructor(
+    private readonly color: Color,
+    file: File,
+    rank: Rank
+  ) {
+    this.position = new Position(file, rank)
+  }
+}
+
+type Color = 'Black' | 'White'
+type File = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H'
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+
+class Position {
+  constructor(
+    private file: File,
+    private rank: Rank
+  ) { }
+}
+
+class King extends Piece { }
+class Queen extends Piece { }
+class Bishop extends Piece { }
+class Knight extends Piece { }
+class Rook extends Piece { }
+class Pawn extends Piece { }
