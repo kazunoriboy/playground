e34e8da29c328e25ce5d8704d34c868eb4c1e5de
kazunoriboy <matunorigmnw@gmail.com>
2022-03-30 09:47:54 +0900

    TypeScriptではクラスの型チェックはクラス名ではなく、クラスの構造で行う

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -695,3 +695,25 @@ class Cat implements Animal, Feline {
     console.info('Meow')
   }
 }
+
+class Zebra {
+  trot() {
+
+  }
+}
+
+class Poodle {
+  trot() {
+
+  }
+}
+
+function ambleAround(animal: Zebra) {
+  animal.trot()
+}
+
+let zebra = new Zebra
+let poodle = new Poodle
+
+ambleAround(zebra)
+ambleAround(poodle)
