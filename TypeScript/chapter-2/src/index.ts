7d9620585e773a1c9e058fc7573f85b00b4adf7e
kazunoriboy <matunorigmnw@gmail.com>
2022-03-31 09:37:24 +0900

    文字列を使った簡易データベースのクラスを作成

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -728,3 +728,24 @@ function f(a: A) {}
 f(new A)
 f(new B)
 f({x: 1})
+
+type State = {
+  [key: string]: string
+}
+
+class StringDatabase {
+  state: State = {}
+  get(key: string): string | null {
+    return key in this.state ? this.state[key] : null
+  }
+  set(key: string, value: string): void {
+    this.state[key] = value
+  }
+  static from(state: State) {
+    let db = new StringDatabase
+    for (let key in state) {
+      db.set(key, state[key])
+    }
+    return db
+  }
+}
