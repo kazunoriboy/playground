319fa90b17407d69ea1966804e56fea494b35e3f
kazunoriboy <matunorigmnw@gmail.com>
2022-04-02 14:28:49 +0900

    Shoeインスタンスを作成するファクトリークラスを作成（ファクトリーメソッドパターン）

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -864,3 +864,13 @@ class Boot implements Shoe {
 class Sneaker implements Shoe {
   purpose = 'walking'
 }
+
+let Shoe = {
+  create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+    switch (type) {
+      case 'balletFlat': return new BalletFlat
+      case 'boot': return new Boot
+      case 'sneaker': return new Sneaker
+    }
+  }
+}
