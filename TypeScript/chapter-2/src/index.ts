d22e6dae685860d130ac13ad132c6c3dc95afcf4
kazunoriboy <matunorigmnw@gmail.com>
2022-04-05 08:55:42 +0900

    スーパータイプとサブタイプの型を定義し、引数に渡す。スーパータイプのみエラーとなる

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -934,3 +934,21 @@ let legacyUser: LegacyUser = {
   name: 'Xin Yang'
 }
 deleteUser(legacyUser)
+
+class Animal {}
+class Bird extends Animal {
+  chirp() {}
+}
+class Crow extends Bird {
+  caw() {}
+}
+
+
+function chirp(bird: Bird): Bird {
+  bird.chirp()
+  return bird
+}
+chirp(new Animal)
+chirp(new Bird)
+chirp(new Crow)
+
