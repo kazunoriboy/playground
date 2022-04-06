7cb2b1f4a89a983f05fff899fcb4aec1d7582480
kazunoriboy <matunorigmnw@gmail.com>
2022-04-06 09:28:41 +0900

    タグ付け用のプロパティを作成し、それで分岐を行うことで、他のプロパティの方を絞り込むことが出来る。この時、同じフィールドに同じ型で一意の値を使用し、ジェネリック型は使用しないようにする

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -1106,13 +1106,13 @@ function parseWidth(width: number | string | null | undefined): Width | null {
   return null
 }
 
-type UserTextEvent = {value: string, target: HTMLInputElement}
-type UserMouseEvent = {value: [number, number], target: HTMLElement}
+type UserTextEvent = {type: 'TextEvent', value: string, target: HTMLInputElement}
+type UserMouseEvent = {type: 'MouseEvent', value: [number, number], target: HTMLElement}
 
 type UserEvent = UserTextEvent | UserMouseEvent
 
 function handle(event: UserEvent) {
-  if (typeof event.value === 'string') {
+  if (event.type === 'TextEvent') {
     event.value
     event.target
 
