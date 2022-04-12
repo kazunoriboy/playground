38a99c86658b3c0dccb90d29a507515491c77d95
kazunoriboy <matunorigmnw@gmail.com>
2022-04-12 09:41:55 +0900

    コンパニオンオブジェクトパターンを使って各型のコンスタラクタを定義

--- TypeScript/chapter-2/src/index.ts
+++ TypeScript/chapter-2/src/index.ts
@@ -1387,6 +1387,18 @@ type OrderID = string & {readonly brand: unique symbol}
 type UserID = string & {readonly brand: unique symbol}
 type ID = CompanyID | OrderID | UserID
 
+function CompanyID(id: string) {
+  return id as CompanyID
+}
+
+function OrderID(id: string) {
+  return id as OrderID
+}
+
+function UserID(id: string) {
+  return id as UserID
+}
+
 function queryForUser(id: UserID) {
 
 }
