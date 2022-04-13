a4750a000cb4ec0e113c707a4dc42cd7903a5514
kazunoriboy <matunorigmnw@gmail.com>
2022-04-13 09:52:04 +0900

    プロンプトを表示し、入力を求める関数を作成

---
+++ TypeScript/chapter-2/src/ErrorHandling.ts
@@ -0,0 +1,23 @@
+function ask() {
+  return prompt('When is your birthday?')!
+}
+
+function parse(birthday: string): Date | null {
+  let date = new Date(birthday)
+  if (!isValid(date)) {
+    return null
+  }
+  return date
+}
+
+function isValid(date: Date) {
+  return Object.prototype.toString.call(date) === '[object Date]'
+    && !Number.isNaN(date.getTime())
+}
+
+let date = parse(ask())
+if (date) {
+  console.info('Date is', date.toISOString())
+} else {
+  console.error('Error parsing date for some reason')
+}
