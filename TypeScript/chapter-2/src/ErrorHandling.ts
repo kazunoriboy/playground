c55fb607c9a463ef26b83392962d4a8361cb72a5
kazunoriboy <matunorigmnw@gmail.com>
2022-04-14 09:14:14 +0900

    カスタムエラー型を作成することで、エラーの種類によって処理を分岐

--- TypeScript/chapter-2/src/ErrorHandling.ts
+++ TypeScript/chapter-2/src/ErrorHandling.ts
@@ -2,10 +2,16 @@ function ask() {
   return prompt('When is your birthday?')
 }
 
+class InvalidDateFormatError extends RangeError {}
+class DateIsInTheFutureError extends RangeError {}
+
 function parse(birthday: string): Date {
   let date = new Date(birthday)
   if (!isValid(date)) {
-    throw new RangeError('Enter a date in the form YYYY/MM/DD')
+    throw new InvalidDateFormatError('Enter a date in the form YYYY/MM/DD')
+  }
+  if (date.getTime() > Date.now()) {
+    throw new DateIsInTheFutureError('Are you a timelord')
   }
   return date
 }
@@ -19,8 +25,10 @@ try {
   let date = parse(ask())
   console.info('Date is', date.toISOString())
 } catch (e) {
-  if (e instanceof RangeError) {
-    console.error('Error parsing date for some reason')
+  if (e instanceof InvalidDateFormatError) {
+    console.error(e.message)
+  } else if (e instanceof RangeError) {
+    console.info(e.message)
   } else {
     throw e
   }
