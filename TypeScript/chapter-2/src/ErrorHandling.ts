95da385712196bdd851771e17f9d31c52f47b838
kazunoriboy <matunorigmnw@gmail.com>
2022-04-16 23:25:16 +0900

    オーバーロードを使ってより具体的な値を指定

--- TypeScript/chapter-2/src/ErrorHandling.ts
+++ TypeScript/chapter-2/src/ErrorHandling.ts
@@ -30,11 +30,14 @@ ask()
   .getOrElse('Error parsing date for some reason')
 
 interface Option<T> {
+  flatMap<U>(f: (value: T) => None): None
   flatMap<U>(f: (value: T) => Option<U>): Option<U>
   getOrElse(value: T): T
 }
 class Some<T> implements Option<T> {
   constructor(private value: T) {}
+  flatMap<U>(f: (value: T) => None): None
+  flatMap<U>(f: (value: T) => Some<U>): Some<U>
   flatMap<U>(f: (value: T) => Option<U>): Option<U> {
     return f(this.value)
   }
@@ -43,7 +46,7 @@ class Some<T> implements Option<T> {
   }
 }
 class None implements Option<never> {
-  flatMap<U>(): Option<U> {
+  flatMap(): None {
     return this
   }
   getOrElse<U>(value: U): U {
