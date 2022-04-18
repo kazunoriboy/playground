dfbd17a9a6a8a7a6efec5bd62289b0e88d8c2a51
kazunoriboy <matunorigmnw@gmail.com>
2022-04-18 09:55:49 +0900

    この呼び出し方だと、ファイルシステムがどれくらいビジーかによって、readFileが返すログが今回の処理を書き込み済みかどうか変わってしまう

--- TypeScript/chapter-2/src/Async.ts
+++ TypeScript/chapter-2/src/Async.ts
@@ -1,3 +1,27 @@
 setTimeout(() => console.info('A'), 1)
 setTimeout(() => console.info('B'), 2)
 console.info('C')
+
+import * as fs from 'fs'
+
+fs.readFile(
+  '/var/log/apache2/access_log',
+  {encoding: 'utf-8'},
+  (error, data) => {
+    if (error) {
+      console.error('error reading!', error)
+      return
+    }
+    console.info('success reading!', data)
+  }
+)
+
+fs.appendFile(
+  'var/log/apache2/access_log',
+  'New access log entry',
+  error => {
+    if (error) {
+      console.error('error writing!', error)
+    }
+  }
+)
