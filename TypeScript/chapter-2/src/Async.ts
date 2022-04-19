0d0ee60f4dfb238aa767919a7ebfcf4331198b6a
kazunoriboy <matunorigmnw@gmail.com>
2022-04-19 09:51:50 +0900

    emitメソッドも同様に型付け

--- TypeScript/chapter-2/src/Async.ts
+++ TypeScript/chapter-2/src/Async.ts
@@ -95,6 +95,10 @@ type RedisClient = {
     event: E,
     f: (arg: Events[E]) => void
   ): void
+  emit<E extends keyof Events>(
+    event: E,
+    arg: Events[E]
+  ): void
 }
 
 type Events = {
