817a5dfd9c50afa51254a11efe133b7bfb2c4d3c
kazunoriboy <matunorigmnw@gmail.com>
2022-04-22 08:57:26 +0900

    メインスレッドから受け取る可能性のある方を合併型で定義し、processCommandFromMainThread関数内で安全に処理をしていく

--- TypeScript/chapter-2/src/WorkerScript.ts
+++ TypeScript/chapter-2/src/WorkerScript.ts
@@ -2,3 +2,23 @@ onmessage = e => {
   console.log(e.data)
   postMessage(`Ack: "${e.data}"`)
 }
+
+type Command = 
+  | {type: 'sendMessageToThread', data: [ThreadID, Message]}
+  | {type: 'createThread', data: [Participants]}
+  | {type: 'addUserToThread', data: [ThreadID, UserID]}
+  | {type: 'removeUserFromThread', data: [ThreadID, UserID]}
+
+onmessage = e => 
+  processCommandFromMainThread(e.data)
+
+function processCommandFromMainThread(
+  command: Command
+) {
+  switch (command.type) {
+    case 'sendMessageToThread':
+      let [threadID, message] = command.data
+      console.log(message);
+      
+  }
+}
