3e43c428de7bb08a6a31694525b0a8a45ca269d8
kazunoriboy <matunorigmnw@gmail.com>
2022-04-21 13:58:12 +0900

    メインスレッドで各種の型とワーカに送るCommandsとワーカから受け取るイベントを定義

--- TypeScript/chapter-2/src/MainThread.ts
+++ TypeScript/chapter-2/src/MainThread.ts
@@ -4,3 +4,22 @@ worker.onmessage = e => {
   console.log(e.data)
   
 }
+
+type Message = string
+type ThreadID = number
+type UserID = number
+type Participants = UserID[]
+
+type Commands = {
+  sendMessageToThread: [ThreadID, Message]
+  createThread: [Participants]
+  addUserToThread: [ThreadID, UserID]
+  removeUserFromThread:  [ThreadID, UserID]
+}
+
+type Events = {
+  receivedMessage: [ThreadID, UserID, Message]
+  createdThread: [ThreadID, Participants]
+  addedUserToThread: [ThreadID, UserID]
+  removedUserFromThread: [ThreadID, UserID]
+}
