c2e1bb08ee7e7a3cac23f7c35de7beb20f693d44
kazunoriboy <matunorigmnw@gmail.com>
2022-04-27 09:47:16 +0900

    コンポーネントの名前を修正

--- TypeScript/my-angular-app/src/app/app.component.ts
+++ TypeScript/my-angular-app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './simple.component.html',
   styleUrls: ['./simple.component.css']
 })
-export class SimpleComponent implements OnInit {
+export class SimpleMessageComponent implements OnInit {
   message?: string
   ngOnInit() {
     this.message = 'No messages, yet'
