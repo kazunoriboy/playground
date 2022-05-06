b5380f8a0aa30674f8dd709cb333ab0481742918
kazunoriboy <matunorigmnw@gmail.com>
2022-05-06 21:52:13 +0900

    switch文を使って条件分岐を作成

--- go/crash_course/06_conditionals/main.go
+++ go/crash_course/06_conditionals/main.go
@@ -21,4 +21,13 @@ func main() {
 	} else {
 		fmt.Println("color is NOT blue or red")
 	}
+
+	switch color {
+	case "red":
+		fmt.Println("color is red")
+	case "blue":
+		fmt.Println("color is blue")
+	default:
+		fmt.Println("color is NOT blue or red")
+	}
 }
