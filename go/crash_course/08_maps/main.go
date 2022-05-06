b20f5c70a51b1723e66754fb4c67ad17768d316f
kazunoriboy <matunorigmnw@gmail.com>
2022-05-07 00:10:32 +0900

    mapの宣言と代入を同時にやった場合の書き方

--- go/crash_course/08_maps/main.go
+++ go/crash_course/08_maps/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	emails := make(map[string]string)
+	// emails := make(map[string]string)
 
-	emails["Bob"] = "bob@example.com"
-	emails["Sharon"] = "sharon@example.com"
-	emails["Mike"] = "mike@example.com"
+	// emails["Bob"] = "bob@example.com"
+	// emails["Sharon"] = "sharon@example.com"
+	// emails["Mike"] = "mike@example.com"
+
+	emails := map[string]string{"Bob": "bob@example.com", "Sharon": "sharon@example.com"}
 
 	fmt.Println(emails)
 	fmt.Println(emails["Bob"])
