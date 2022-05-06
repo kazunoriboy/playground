8dbda3c64cdba1dddf1fef58290de71172a7e836
kazunoriboy <matunorigmnw@gmail.com>
2022-05-06 19:09:14 +0900

    float64に推論される

--- go/app/go_crash_course/02_vars/main.go
+++ go/app/go_crash_course/02_vars/main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	name := "Brad"
 	var age int32 = 37
-	var isCool = true
-	isCool = false
+	// var isCool = true
+	// isCool = false
+	size := 1.3
 
 	fmt.Println(name, age)
-	fmt.Printf("%T\n", isCool)
+	fmt.Printf("%T\n", size)
 }
