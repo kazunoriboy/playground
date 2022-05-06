716857fdbd5e1af6257275b6fa50a006ba64d095
kazunoriboy <matunorigmnw@gmail.com>
2022-05-06 19:12:52 +0900

    ワンラインで復数の変数を宣言することが可能

--- go/app/go_crash_course/02_vars/main.go
+++ go/app/go_crash_course/02_vars/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	name := "Brad"
+	// name := "Brad"
+	// email := "brad@example.com"
+	name, email := "Brad", "brad@example.com"
 	var age int32 = 37
 	// var isCool = true
 	// isCool = false
 	var size float32 = 2.4
 
-	fmt.Println(name, age)
+	fmt.Println(name, age, email)
 	fmt.Printf("%T\n", size)
 }
