7c92b294327a0fc38a3c9cb8eacca44cf2f5fd11
kazunoriboy <matunorigmnw@gmail.com>
2022-05-06 21:26:42 +0900

    配列を定義、コメントインしている方は宣言と代入を同時に行っている

---
+++ go/crash_course/05_arrays_slices/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var fruitArr [2]string
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Orange"
+
+	fruitArr := [2]string{"Apple", "Orange"}
+
+	fmt.Println(fruitArr)
+	fmt.Println(fruitArr[0])
+
+}
