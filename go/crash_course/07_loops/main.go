66825e4f6db4295f224fb13c503c4ba9ac464613
kazunoriboy <matunorigmnw@gmail.com>
2022-05-06 21:59:40 +0900

    for とifを組み合わせてfizz buzzを実装

--- go/crash_course/07_loops/main.go
+++ go/crash_course/07_loops/main.go
@@ -12,4 +12,16 @@ func main() {
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("Number %d\n", i)
 	}
+
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
