dfc4587e9dfda14442a9cddf8305465bac2182e2
kazunoriboy <matunorigmnw@gmail.com>
2022-05-06 21:01:19 +0900

    数字を足し合わせる関数を作成

--- go/crash_course/04_functions/main.go
+++ go/crash_course/04_functions/main.go
@@ -6,6 +6,10 @@ func greeting(name string) string {
 	return "Hello " + name
 }
 
+func getSum(num1 int, num2 int) int {
+	return num1 + num2
+}
+
 func main() {
-	fmt.Println(greeting("john"))
+	fmt.Println(getSum(3, 4))
 }
