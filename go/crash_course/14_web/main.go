ee0d25fb9f2c9cbe93613bf72bb7443def5a2b02
kazunoriboy <matunorigmnw@gmail.com>
2022-05-10 09:52:50 +0900

    別のルーティングを作成

--- go/crash_course/14_web/main.go
+++ go/crash_course/14_web/main.go
@@ -7,10 +7,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
 	fmt.Println("Server starting...")
 	http.ListenAndServe(":80", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>About</h1>")
 }
