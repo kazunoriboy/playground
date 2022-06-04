a235d6dc95e2556ee71e978b4656a4a2eec03273
kazunoriboy <matunorigmnw@gmail.com>
2022-06-04 16:33:39 +0900

    残りのルートも全て定義（実装は未実装）

--- go/app/main.go
+++ go/app/main.go
@@ -11,6 +11,18 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
