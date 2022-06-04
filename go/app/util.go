85b4ff7868dcb2162708c43daf6290d416dbcd44
kazunoriboy <matunorigmnw@gmail.com>
2022-06-04 17:20:07 +0900

    sessionでログインチェックを行うユーティリティ関数を作成

---
+++ go/app/util.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+)
+
+func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = data.Session{Uuid: cookie.Value}
+		if ok, _ := sess.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+	return
+}
