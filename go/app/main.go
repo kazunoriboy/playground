f8a044d9d473835111f3d2d6ca41ce141b521bfe
kazunoriboy <matunorigmnw@gmail.com>
2022-06-04 16:19:13 +0900

    サーバの起動

--- go/app/main.go
+++ go/app/main.go
@@ -10,4 +10,10 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
 	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
