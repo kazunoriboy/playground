151ec81d604e52a2fa09e8b7ca81262c90481cc0
kazunoriboy <matunorigmnw@gmail.com>
2023-08-24 00:54:56 +0900

    change AMI

--- terraform/learn-terraform-aws-instance/main.tf
+++ terraform/learn-terraform-aws-instance/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0d52744d6551d851e"
+  ami           = "ami-0a6128af4a22cbdf2"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-05929e18718e83ef8"]
   subnet_id = "subnet-023e6ac789806a264"
