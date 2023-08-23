9b6877727bc2d09e887f44c605cc5112a37b078a
kazunoriboy <matunorigmnw@gmail.com>
2023-08-24 00:41:54 +0900

    build aws instance.
    vpc, subnet and security group was created by admin console.

---
+++ terraform/learn-terraform-aws-instance/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d52744d6551d851e"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-05929e18718e83ef8"]
+  subnet_id = "subnet-023e6ac789806a264"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+
